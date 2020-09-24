6a1acca05725220ef36a8b4287ec8bf99e2370c1
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-24 15:19:31 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace jurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capital;
+            double taxa;
+            double resultTaxa;
+            double tempo;
+            double juros;
+            double montante;
+
+            Console.WriteLine("digite seu valor r$:");
+            capital = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite sua taxa de juros %:");
+            taxa =  double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite seu tempo de parcelas:");
+            tempo = double.Parse(Console.ReadLine());
+            Console.Write("/n");
+
+            resultTaxa = taxa/ 100;
+            juros =capital * resultTaxa *  tempo;
+            Console.WriteLine($"seu juros é igual = {0}", juros);
+
+            montante = capital + juros;
+            Console.WriteLine("seu montante é = {0}", montante);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
